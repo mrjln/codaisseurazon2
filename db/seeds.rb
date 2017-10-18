2127d68cbc997c059c3e1f7b732533a219ebab1f
mrjln <marjolein_tromp@hotmail.com>
2017-10-18 21:30:07 +0200

    declared relations between models

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#Create some products
+Product.destroy_all
+product_1 = Product.create!(
+  name: "Kangaroo",
+  description: " dsljgsadlkgjlsakdgjlksadgj",
+  price: 56.50
+)
+product_2 = Product.create!(
+name: "Elephant",
+description: "sadglasjkdglasdjgl;sadkjgas",
+price: 23
+)
+product_3 = Product.create!(
+name: "Giraffe",
+description: "sdgjasdkvjads;lkjv",
+price: 423
+)
+product_4 = Product.create!(
+name: "Elf",
+description: "Ajdlsdjlsadjfasdgasg",
+price: 5325
+)
