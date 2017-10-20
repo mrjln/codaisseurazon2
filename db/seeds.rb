f4e35210739367285938135b155b57636a1e74fb
sjki-l <sjkilouwerse@gmail.com>
2017-10-20 09:37:25 +0200

    Added methods to order and order_item models

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,7 @@
 #Create some products
 Product.destroy_all
-product_1 = Product.create!(
+
+product_1 = Product.create!({
   name: "Kangaroo",
   description: "The kangaroo is a marsupial from the family Macropodidae
   (macropods, meaning 'large foot'). In common use the term is used to
@@ -10,9 +11,9 @@ product_1 = Product.create!(
     Australia. The Australian government estimates that 34.3 million kangaroos
     lived within the commercial harvest areas of Australia in 2011, up from
      25.1 million one year earlier.",
-  price: 56.50
-)
-product_2 = Product.create!(
+  price: 56.50 })
+
+product_2 = Product.create!({
 name: "Elephant",
 description: "Elephants are large mammals of the family Elephantidae and
 the order Proboscidea. Three species are currently recognized: the African
@@ -20,15 +21,14 @@ bush elephant (Loxodonta africana), the African forest elephant (L. cyclotis),
  and the Asian elephant (Elephas maximus). Elephants are scattered throughout
   sub-Saharan Africa, South Asia, and Southeast Asia. Elephantidae is the only
    surviving family of the order Proboscidea; other, now extinct, ",
-price: 23
-)
-product_3 = Product.create!(
+price: 23 })
+
+product_3 = Product.create!({
 name: "Giraffe",
 description: "The giraffe (Giraffa) is a genus of African even-toed ungulate mammals, the tallest living terrestrial animals and the largest ruminants. The genus currently consists of one species, Giraffa camelopardalis, the type species. Seven other species are extinct, prehistoric species known from fossils. Taxonomic classifications of one to eight extant giraffe species have been described, based upon research into the mitochondrial and nuclear DNA, as well as morphological measurements of Giraffa, but the IUCN currently recognizes only one species with nine subspecies.",
-price: 423
-)
-product_4 = Product.create!(
+price: 423 })
+
+product_4 = Product.create!({
 name: "Elf",
 description: "An elf (plural: elves) is a type of human-shaped supernatural being in Germanic mythology and folklore. In medieval Germanic-speaking cultures, elves seem generally to have been thought of as beings with magical powers and supernatural beauty, ambivalent towards everyday people and capable of either helping or hindering them.[1] However, the details of these beliefs have varied considerably over time and space, and have flourished in both pre-Christian and Christian cultures.",
-price: 5325
-)
+price: 5325 })
