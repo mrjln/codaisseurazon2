89e254865938e12a5c74c5ab10eef4d34d577b1c
mrjln <marjolein_tromp@hotmail.com>
2017-10-20 16:27:49 +0200

    Added show products to cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,11 +1,11 @@
 class CartsController < ApplicationController
 
   def show
-    @current_cart
+  #  @current_cart
   end
 
   def index
-    @order_items = current_order.order_items
+    #@order_items = current_order.order_items
   end
 
   def update
